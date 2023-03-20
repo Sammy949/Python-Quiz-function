5d068b8958e673806a56cf0502fba78b7e79f7c9
SamY <91989013+Sammy949@users.noreply.github.com>
2023-03-20 01:42:54 +0100

    Update q_a.py

--- q_a.py
+++ q_a.py
@@ -8,10 +8,11 @@ def quiz(question, answer):
         print("Wrong")
         print(f"The correct answer is {answer}")
 
-question1 = input("What is my Name? ")
-quiz(question1, "Samuel")
+question1 = input("What is the capital of the United States of America? ")
+quiz(question1, "Washington DC")
 
-question2 = input("How old am I? ")
-quiz(question2, "13")
+question2 = input("Who is the present ruler of England? ")
+quiz(question2, "King Charles III")
 
 print(f"Your score is {score}")
+print("Thanks for checking it out. samy01.netlify.app")
